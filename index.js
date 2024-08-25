85b18eb0aa17ecca138e1b430cda7f22d0f17cdc
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-08-25 17:25:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// run `node index.js` in the terminal
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log("Starting application...");
+
+mongoose 
+.connect(config.mongoose.url)
+.then(() => {
+    console.log("Connected to database")
+    const server = app.listen(config.port, () => {
+        console.log(`Server listening on ${config.port}`)
+    })
+})
+.catch((err) => console.log(err));
